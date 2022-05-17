82d87148b1a1a6a0ee7424a8acdcc50f0664ba26
calvin kwan <ckw28502@gmail.com>
2022-05-17 12:51:42 +0700

    Chart member jadi

--- public/halamanchartperkembangan.php
+++ public/halamanchartperkembangan.php
@@ -92,7 +92,7 @@
         <script src="../utils/jquery-3.6.0.min.js"></script>
         <script>
             $(document).ready(function () {
-                console.log("HI");
+                var chart;
                 $.ajax({
                     method:"get",
                     url:"../controllers/trans.php",
@@ -117,17 +117,64 @@
                         action:"getbyyear",
                         tahun:document.getElementById("tahun").value
                     }
-                }).done((data)=>{
-                    let temp=JSON.parse(data);
-                    console.log("hi");
+                }).done((d)=>{
+                    let temp=JSON.parse(d);
                     const bulan=["Jan","Feb","Mar","Apr","Mei","Jun","Jul","Agt","Sep","Okt","Nov","Des"];
-                    let k=1;
                     let qty=[];
-                    console.log(bulan);
-                    console.log(qty);
+                    for (let index = 1; index < 13; index++) {
+                        let bulan_qty=0;
+                        temp.forEach(el => {
+                            if (el["bulan"]==index) {
+                                bulan_qty=el["qty"];
+                            }
+                        });
+                        qty.push(bulan_qty);
+                    }
+                    const labels = bulan;
+                    const data = {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Bulan',
+                            data: qty,
+                            backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 205, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(201, 203, 207, 0.2)'
+                            ],
+                            borderColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(255, 159, 64)',
+                            'rgb(255, 205, 86)',
+                            'rgb(75, 192, 192)',
+                            'rgb(54, 162, 235)',
+                            'rgb(153, 102, 255)',
+                            'rgb(201, 203, 207)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    };
+                    const config={
+                        type:"bar",
+                        data: data,
+                        options: {
+                            scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                            }
+                        },
+                    };
+                    chart=new Chart($("canvas"),config);
                 });
                 };
-                $("select").on("change",getdata);
+                $("select").on("change",function () {
+                    chart.destroy();
+                    getdata();
+                });
             });
         </script>
     </body>
