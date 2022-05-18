13060e2590fe90c5c974e67bd55bd24ec536f4b3
TimothyAxell <timothy_a20@mhs.istts.ac.id>
2022-05-18 20:50:24 +0700

    rapikan video

--- controllers/auth.php
+++ controllers/auth.php
@@ -54,7 +54,7 @@ if(isset($_POST["login"])){
                         header("Location: ../public/halamanuserbiasa.php");
                         exit;
                     }
-                }
+                } 
             }
             else{
                 header("Location: ../index.php");
