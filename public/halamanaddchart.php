60b86277530cd544e65de8054434de68587aed56
calvin kwan <ckw28502@gmail.com>
2022-05-23 10:49:36 +0700

    perbaikan chart

--- public/halamanaddchart.php
+++ public/halamanaddchart.php
@@ -137,7 +137,6 @@
                         },
                     }).done((data)=>{  
                         $("#modal").html(data); 
-                        $()
                         cekupdate();
                     })
                 }
